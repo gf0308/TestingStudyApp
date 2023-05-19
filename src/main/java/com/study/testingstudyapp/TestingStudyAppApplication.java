aab082c762115f25f2797dc3cc7358dc7986a137
gf0308 <kjbk0308@gmail.com>
2023-05-19 23:42:32 +0900

    init git commit of TestingStudyApp

---
+++ src/main/java/com/study/testingstudyapp/TestingStudyAppApplication.java
@@ -0,0 +1,13 @@
+package com.study.testingstudyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingStudyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingStudyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/testingstudyapp/TestingStudyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.testingstudyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingStudyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
